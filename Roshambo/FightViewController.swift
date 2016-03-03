1d59c8f11352e142958995112865e269f3727ab4
Julian Scharf <julzelements@gmail.com>
2016-03-03 19:10:47 +1000

    Initial Commit

---
+++ Roshambo/FightViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FightViewController.swift
+//  Roshambo
+//
+//  Created by Julian Scharf on 3/03/2016.
+//  Copyright © 2016 Julian Scharf. All rights reserved.
+//
+
+import UIKit
+
+class FightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Roshambo/RoshamboBrain.swift
@@ -0,0 +1,20 @@
+//
+//  RoshamboBrain.swift
+//  Roshambo
+//
+//  Created by Julian Scharf on 3/03/2016.
+//  Copyright © 2016 Julian Scharf. All rights reserved.
+//
+
+import Foundation
+
+class RoshshamboBrain {
+  
+  enum userHand {
+    case rock
+    case paper
+    case scissors
+  }
+  
+  
+}
